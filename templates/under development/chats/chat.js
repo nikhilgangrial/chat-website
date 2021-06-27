b88250f43cee40f3256505199ec4a2fdc40c6eaf
nikhilgangrial <nikhilgangrial@gmail.com>
2021-06-27 12:40:11 +0530

    chat page is responsive now

--- templates/under development/chats/chat.js
+++ templates/under development/chats/chat.js
@@ -1,7 +1,14 @@
-var k = 100000 -1;
 
-function move(ele){
-    ele.setAttribute('style', "order: " + k );
-    k -= 1;
-
+function extralargescreen() {
+    if (window.innerHeight > 1080) {
+        document.children[0].setAttribute("style", "font-size: " + (window.innerHeight / 1080) + "em;");
+    }else if (window.innerHeight < 577) {
+        document.children[0].setAttribute("style", "font-size: " + (window.innerWidth / 577) + "em;");
+    }
+    else {
+        document.children[0].setAttribute("style", "font-size: 1em");
+    }
+    setTimeout(extralargescreen, 1000);
 }
+
+setTimeout(extralargescreen, 1000);
